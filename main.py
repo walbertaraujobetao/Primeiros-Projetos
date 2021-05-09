c15ea1085e6a7ee5908df60b64f3ed6d76d1469f
walbertaraujobetao <wap.beto@gmail.com>
2021-05-09 02:05:05 -0300

    inclusão do except

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ class SimuladorDeDado:
     
     def Iniciar(self): # criando método para iniciar
         resposta = input(self.mensagem) # vamos guar a resposta do usuário com um input
-        if resposta == 'sim' or resposta == 's':
-         self.GerarValorDoDado() # sempre criar um método com nome simples, para ficar de fácil compreensão
-        elif resposta == 'não' or resposta == 'n':
-            print('Agradecemos sua participação!')
-        else:
-            print('Favor digitar: Sim ou não!')
-                
+        try:
+            if resposta == 'sim' or resposta == 's':
+                self.GerarValorDoDado() # sempre criar um método com nome simples, para ficar de fácil compreensão
+            elif resposta == 'não' or resposta == 'n':
+                print('Agradecemos sua participação!')
+            else:
+                print('Favor digitar: Sim ou não!')
+        except:
+            print('Ocorreu um erro ao receber sua resposta!')
+        
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo,self.valor_maximo)) # chamar a função d o random para ser impressa
         
