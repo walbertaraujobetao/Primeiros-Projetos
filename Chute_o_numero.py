0ad206ffc3542ad91acdcef54eddbb9fe84176e2
walbertaraujobetao <wap.beto@gmail.com>
2021-05-09 18:32:17 -0300

    Inicio projeto 3

---
+++ Chute_o_numero.py
@@ -0,0 +1,9 @@
+# projeto 3 - Chute o número
+# Objetivo: Criar um algorítimo que gera um valor aleatório, e o usuário tem que ir tentando
+# até acertar o número gerado pelo computador!
+
+import random
+
+class ChuteONumero:
+    def __init__(self):
+        self.valor_aleatorio = 
