2b9accf04722152aaaed250283be1222ba37b20d
walbertaraujobetao <wap.beto@gmail.com>
2021-05-09 18:41:55 -0300

    defs

--- Chute_o_numero.py
+++ Chute_o_numero.py
@@ -6,4 +6,12 @@ import random
 
 class ChuteONumero:
     def __init__(self):
-        self.valor_aleatorio = 
+        self.valor_aleatorio = 0
+        self.valor_minimo = 1
+        self.valor_maximo = 100
+        
+    def Iniciar(self):
+        self.GerarNumeroAleatorio()
+        
+    def GerarNumeroAleatorio(self):
+        
