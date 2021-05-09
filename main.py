fc15aa732abed4de06d090b2d494112a395e3c33
walbertaraujobetao <wap.beto@gmail.com>
2021-05-09 18:07:43 -0300

    janela_1

--- main.py
+++ main.py
@@ -10,24 +10,27 @@ class SimuladorDeDado:
         self.mensagem = 'Você gostaria de gerar um novo valor para o dado: '
         # layout
         layout = [
-            []
-
+            [sg.Text('Jogar o Dado?')]
+            [sg.Button('Sim'), sg.Button('Não')]
         ]
-        # Criar uma janela
-        # ler os valores da tela
-        # fazer alguma coisa com esses valores
-
+        
     def Iniciar(self):  # criando método para iniciar
-        resposta = input(self.mensagem)  # vamos guar a resposta do usuário com um input
-        try:
-            if resposta == 'sim' or resposta == 's':
-                self.GerarValorDoDado()  # sempre criar um método com nome simples, para ficar de fácil compreensão
-            elif resposta == 'não' or resposta == 'n':
-                print('Agradecemos sua participação!')
-            else:
-                print('Favor digitar: Sim ou não!')
-        except:
-            print('Ocorreu um erro ao receber sua resposta!')
+        # Criar uma janela
+        self.janela = sg.Window('Simulador de Dado',layout=layout]
+        # ler os valores da tela 
+        self.eventos, self.valores = janela.Read()
+        # fazer alguma coisa com esses valores
+        While True:
+            try:
+                sel.eventos == 'sim':
+                    if resposta == 'sim' or resposta == 's':
+                        self.GerarValorDoDado()  # sempre criar um método com nome simples, para ficar de fácil compreensão
+                    elif resposta == 'não' or resposta == 'n':
+                        print('Agradecemos sua participação!')
+                    else:
+                        print('Favor digitar: Sim ou não!')
+            except:
+                print('Ocorreu um erro ao receber sua resposta!')
 
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo))  # chamar a função d o random para ser impressa
