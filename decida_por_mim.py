bcd653bdbcbccf0629a11acdf42115f0b4d91f4d
walbertaraujobetao <wap.beto@gmail.com>
2021-05-10 17:10:15 -0300

    parte principal pronta

--- decida_por_mim.py
+++ decida_por_mim.py
@@ -1,1 +1,22 @@
 # Faça uma pergunta para o programa e ele teráque te dar uma resposta!
+
+import random
+
+
+class DecidaPorMim:
+    def __init__(self):
+        self.respostas = [
+            'Com certeza, você deve fazer isso!',
+            'Não sei, você que sabe!',
+            'Não faz isso não!',
+            'Acho que está na hora certa!',
+            'Talvez...'
+        ]
+
+    def Iniciar(self):
+        input('Faça sua pergunta: ')
+        print(random.choice(self.respostas))
+
+
+decida = DecidaPorMim()
+decida.Iniciar()
