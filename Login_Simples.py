7649242f5aa20f0c103dc1caaa6d93f2067f4aa7
walbertaraujo <wap.beto@gmail.com>
2021-05-21 15:05:43 -0300

    Fim!

--- Login_Simples.py
+++ Login_Simples.py
@@ -1,10 +1,11 @@
 usuario = input('Digite seu nome: ').strip().upper()
 senha = int(input('Digite sua senha: '))
 
-usuario_bd = BETO
+usuario_bd = 'BETO'
 senha_bd = 123456
 
 if usuario_bd == usuario and senha_bd == senha:
     print('Você está logado!')
-    else:
+else:
+    print('Nome ou senha incorretos!')
         
